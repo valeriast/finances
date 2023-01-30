5e60ff8e1610de6968a059dd030a5d9b6587aaba
Valeria <valeria.s@outlook.com.br>
2023-01-29 23:01:44 -0300

    added receives api logic

---
+++ src/components/HistoryList/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, TipoText } from './styles'
+
+export default function HistoryList({data}) {
+ return (
+   <Container>
+     <TipoText> Teste tipo </TipoText>
+   </Container>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,14 @@ import React, {useContext, useEffect, useState} from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { AuthContext } from '../../contexts/auth';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import api from '../../services/api'
 
-import { Background, ListBalance } from './styles'
+import { Background, ListBalance, Area,  Title, List } from './styles'
 import Header from '../../components/Header'
 import BalanceItem from '../../components/BalanceItem';
+import HistoryList from '../../components/HistoryList'
 
 import { format } from 'date-fns';
 import { useIsFocused } from '@react-navigation/native';
@@ -15,6 +17,7 @@ import { useIsFocused } from '@react-navigation/native';
 
 export default function Home() {
   const [listBalance, setListBalance] = useState([])
+  const [movements, setMovements] = useState([])
   const [dateMovements, setDateMovements] = useState(new Date())
   const isFocused = useIsFocused()
   
@@ -23,11 +26,18 @@ export default function Home() {
     async function getMovements(){
       let dateFormated = format(dateMovements, 'dd/MM/yyyy');
 
+      const receives = await api.get('/receives', {
+        params:{
+          date: dateFormated
+        }
+      })
+
       const balance = await api.get('/balance', {
         params:{
           date: dateFormated 
         }
       })
+      setMovements(receives.data)
       setListBalance(balance.data);
     }
 
@@ -44,6 +54,19 @@ export default function Home() {
         showsHorizontalScrollIndicator={false}
         keyExtractor={ item => item.tag}
         renderItem={({item}) => (<BalanceItem data={item}/>)}
+      />
+      <Area>
+        <TouchableOpacity>
+          <Icon name="event" color="#121212" size={30}/>
+        </TouchableOpacity>
+        <Title>Ultimas movimentações</Title>
+      </Area>
+      <List
+        data={movements}
+        keyExtractor={ item=> item.id}
+        renderItem={({item}) => <HistoryList data={item}/>}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 20}}
       />
    </Background>
   );
